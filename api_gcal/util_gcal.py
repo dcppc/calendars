79785ea5c0daa0735a52aa4a49d48aa6ea00de49
Charles Reid <charlesreid1@gmail.com>
2018-09-07 11:56:47 -0700

    update google calendar utilities to reflect ical utilities

--- api_gcal/util_gcal.py
+++ api_gcal/util_gcal.py
@@ -12,38 +12,31 @@ Google Calendar API Utilities
 
 Description:
 
-This file contains utility methods for interfacing
-with the Google Calendar API.
-
+    This file contains utility methods for interfacing
+    with the Google Calendar API.
 
 Utility Methods:
 
-get_service() - provides an API object (service) to allow
-interaction with the Google Calendar API.
-
-save_cal_id() - save the calendar id to the calendar id file.
-
-load_cal_id() - load the calendar id from the calendar id file.
-
+    get_service() - provides an API object (service) to allow
+    interaction with the Google Calendar API.
+    
+    save_cal_id() - save the calendar id to the calendar id file.
+    
+    load_cal_id() - load the calendar id from the calendar id file.
 
 Calendar Methods:
 
-create_calendar() - create a calendar, take a summary and
-time zone as arguments, save to calendar id file.
-
-get_event_ids() - get a set of event ids from a specified
-calendar id.
-
-
-
-
+    create_calendar() - create a calendar, take a summary and
+    time zone as arguments, save to calendar id file.
+    
+    get_event_ids() - get a set of event ids from a specified
+    calendar id.
 
 Constants:
 
-
-CAL_ID_FILE - the name of the file containing the
-id of the temporary calendar created. Used to modify
-events on the same calendar later.
+    CAL_ID_FILE - the name of the file containing the
+    id of the temporary calendar created. Used to modify
+    events on the same calendar later.
 """
 
 
@@ -56,6 +49,16 @@ def get_service():
     See pydocs here: 
     https://developers.google.com/resources/api-libraries/documentation/calendar/v3/python/latest/
     """
+    if not os.path.exists('credentials.json'):
+        print("Could not find OAuth credentials in credentials.json.")
+        if not os.path.exists('client_secrets.json'):
+            print("Could not find API credentials in client_secrets.json")
+
+            err = "Error: no API credentials for Google Calendar!\n"
+            err += "Download client_secrets.json or credentials.json from the "
+            err += "Google Cloud console."
+            raise Exception(err)
+
     SCOPES = 'https://www.googleapis.com/auth/calendar'
     store = file.Storage('credentials.json')
     creds = store.get()
@@ -95,7 +98,7 @@ def load_cal_id():
     return calendar_id
 
 
-def create_calendar(summary,timeZone="America/New_York"):
+def create_gcal(summary,timeZone="America/New_York"):
     """
     Create a calendar with a brief description in summary
     and specifying the time zone.
@@ -110,60 +113,80 @@ def create_calendar(summary,timeZone="America/New_York"):
         err = "ERROR: Calendar summary was empty"
         raise Exception(err)
 
+    if os.path.exists(CAL_ID_FILE):
+        # Already created a calendar, so load its id
+        print("Found a calendar ID file at %s"%(CAL_ID_FILE))
+        return
+
+    else:
+        # Create a new calendar
+
+        # Get API
+        service = get_service()
+
+        calendar_id = None
+        try:
+            calendar = {
+                'summary': summary,
+                'timeZone': timeZone,
+            }
+            print("Creating calendar file %s"%(CAL_ID_FILE))
+            created_calendar = service.calendars().insert(body=calendar).execute()
+            calendar_id = created_calendar['id']
+            save_cal_id(calendar_id)
+            print("Done. Created calendar with id: %s"%(calendar_id))
+
+        except client.AccessTokenRefreshError:
+            err = 'ERROR: Could not create test calendar\n'
+            err += 'The credentials have been revoked or expired, please re-run '
+            err += 'the application to re-authorize.'
+            raise Exception(err)
+
+        print("Finished creating a calendar id file at %s"%(CAL_ID_FILE))
+
+
+
+def destroy_gcal(calendar_id):
+    # Get an instance of cal calendar_id
+    # and delete the calendar
+    # (don't forget to delete the cal id file too)
+    pass
+
+
+
+def populate_gcal_from_components_map(calendar_id, components_map):
+    # This is where we get an instance of cal calendar_id
+    # and iterate through each event in components_map
+    # and add each event to the calendar
+    pass
+
+
+
+
+
+def gcal_components_map(calendar_id, components_map={}):
+    for e in gcal_components_generator(calendar_id):
+        components_map[e['id']] = e
+    return components_map
+
+
+
+def gcal_components_generator(calendar_id):
+    """
+    Given a calendar id as a string,
+    get the calendar and iterate through
+    each event (component), yielding
+    the JSON for the object as we go
+    """
     # Get API
     service = get_service()
 
-    calendar_id = None
-    try:
-        calendar = {
-            'summary': summary,
-            'timeZone': timeZone,
-        }
-        print("Creating calendar...")
-        created_calendar = service.calendars().insert(body=calendar).execute()
-        calendar_id = created_calendar['id']
-        print("Done. Created calendar with id: %s"%(calendar_id))
-
-    except client.AccessTokenRefreshError:
-        err = 'ERROR: Could not create test calendar\n'
-        err += 'The credentials have been revoked or expired, please re-run '
-        err += 'the application to re-authorize.'
-        raise Exception(err)
-
-
-
-def get_event_ids(calendar_id):
-    """
-    Return a set containing all unique event IDs
-    for events on this particular calendar.
-    """
-    # Get API
-    service = get_service()
-
-    event_ids = set()
-
-    page_token = None
     while True:
-
-        calendar_list = service.calendarList().list(pageToken=page_token).execute()
-        for calendar_list_entry in calendar_list['items']:
-
-            cal_id = calendar_list_entry['id']
-
-            max_time = '2018-11-01T00:00:00Z'
-            events_list = service.events().list(calendarId=cal_id, timeMax=max_time).execute()
-            for events_list_entry in events_list['items']:
-
-                event_ids.add(events_list_entry['id'])
-
-        page_token = calendar_list.get('nextPageToken')
+        events_list = service.events().list(calendarId=calendar_id, timeMax=FUTURE).execute()
+        for events_list_entry in events_list['items']:
+            yield events_list_entry
+        page_token = events_list.get('nextPageToken')
         if not page_token:
             break
-
-    print("Extracted events from calendar %s:"%(calendar_id))
-    print(event_ids)
-    return event_ids
-
-
 
 
